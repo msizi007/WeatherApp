3a99da2584bfb601f141a99137a46c8dfd5183e2
Msizi <msizimwelase007@gmail.com>
2025-09-25 11:55:10 +0200

    Added search functionality and current location functionality using WeatherAPI.com (API) from RapidAPI

--- src/App.tsx
+++ src/App.tsx
@@ -3,40 +3,81 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import WeatherCard from "./components/WeatherCard/WeatherCard";
 import Searchbar from "./components/Searchbar/Searchbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
   const [latitude, setLatitude] = useState(0);
   const [longitude, setLongitude] = useState(0);
   const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState<any>(null);
+  const [search, setSearch] = useState("");
 
-  function getUserLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const lat = position.coords.latitude;
-          const lon = position.coords.longitude;
-          console.log("Latitude:", lat);
-          console.log("Longitude:", lon);
-          setLatitude(lat);
-          setLongitude(lon);
-
-          // You can now use lat & lon to call your weather API
-          axios
-        },
-        (error) => {
-          console.error("Error getting location:", error.message);
+  const fetchWeather = async (q: string, days: number) => {
+    try {
+      const response = await axios.get(
+        "https://weatherapi-com.p.rapidapi.com/forecast.json",
+        {
+          params: {
+            q: q,
+            days: days,
+          },
+          headers: {
+            "X-RapidAPI-Key":
+              "179dfa5fd3msh772857b01a679bap11c067jsnbcd94abadcc8",
+            "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+          },
         }
       );
-    } else {
-      alert("Geolocation is not supported by this browser.");
+      console.log("Weather data:", response.data);
+      setCity(response.data.location.name);
+      setWeatherData(response.data);
+    } catch (err) {
+      console.error("Failed to fetch weather:", err);
     }
-  }
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        console.log("Latitude:", latitude);
+        console.log("Longitude:", longitude);
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (error) => {
+        console.error("Error getting location:", error.message);
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    if (latitude && longitude) {
+      fetchWeather(`${latitude},${longitude}`, 7);
+    }
+  }, [latitude, longitude]);
+
   return (
     <>
-      <Header location="Cape Town" />
-      <Searchbar />
-      <WeatherCard day="Today" icon={FaSun} temperature={0} status="Sunny" />
+      {weatherData ? (
+        <>
+          <Header location={city} />
+          <Searchbar
+            input={search}
+            setInput={setSearch}
+            onSearch={() => fetchWeather(search, 7)}
+          />
+          <WeatherCard
+            day="Today"
+            icon={FaSun}
+            temperature={Math.round(weatherData.current.temp_c)}
+            status="Sunny"
+          />
+        </>
+      ) : (
+        <h2>Loading...</h2>
+      )}
     </>
   );
 }
--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,15 +1,22 @@
 import { BiSearch } from "react-icons/bi";
 import styles from "./searchbar.module.css";
 
-export default function Searchbar() {
+interface Props {
+  input: string;
+  setInput: React.Dispatch<React.SetStateAction<string>>;
+  onSearch: () => void;
+}
+export default function Searchbar(props: Props) {
   return (
     <div className={styles.searchbar}>
       <input
         className={styles.input}
         type="text"
         placeholder="search for a city..."
+        value={props.input}
+        onChange={(e) => props.setInput(e.target.value)}
       />
-      <BiSearch className={styles.icon} size={30} />
+      <BiSearch className={styles.icon} size={30} onClick={props.onSearch} />
     </div>
   );
 }
