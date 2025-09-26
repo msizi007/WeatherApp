f00187bbcfe2183ac7b958cd020b01ecbbc0c0bc
Msizi <msizimwelase007@gmail.com>
2025-09-26 13:14:43 +0200

    updated favourite icon from a pin icon to a star icon

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { getCities, type City } from "../../utils/storage";
 import styles from "./sidebar.module.css";
 
 interface Props {
@@ -5,6 +6,11 @@ interface Props {
   setIsDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
   showMenu: boolean;
   setShowMenu: React.Dispatch<React.SetStateAction<boolean>>;
+  isCelcius: boolean;
+  setIsCelcius: React.Dispatch<React.SetStateAction<boolean>>;
+  setSearchInput: React.Dispatch<React.SetStateAction<string>>;
+  setCityChange: React.Dispatch<React.SetStateAction<string>>;
+  fetchWeather: (q: string, days: number) => void;
 }
 export default function Sidebar(props: Props) {
   return (
@@ -30,19 +36,40 @@ export default function Sidebar(props: Props) {
             <span className={`${styles.slider} ${styles.round}`}></span>
           </label>
         </span>
+
         <span className={styles.unitFilter}>
           <span className={styles.text}>Celcius (°C)</span>
           <label className={styles.switch}>
             <input
               className={styles.input}
               type="checkbox"
-              onClick={() => props.setIsDarkMode(!props.isDarkMode)}
+              onClick={() => props.setIsCelcius(!props.isCelcius)}
             />
             <span className={`${styles.slider} ${styles.round}`}></span>
           </label>
           <span className={styles.text}>Fahrenheit (°F)</span>
         </span>
       </div>
+      <h2 className={styles.title}>Favourite Cities</h2>
+      <hr className={styles.hr} />
+      {getCities()
+        .filter((city: City) => city.isPinned)
+        .map((city: City) => (
+          <button
+            className={styles.button}
+            key={city.name}
+            onClick={() => {
+              console.log("City", city.name);
+
+              props.setSearchInput(city.name);
+              props.setCityChange(city.name);
+              props.fetchWeather(city.name, 7);
+              props.setShowMenu(false);
+            }}
+          >
+            {city.name}
+          </button>
+        ))}
       {/* <div className={styles.sidebarMenus}>
           {JSON.parse(localStorage.getItem("cities")!).map(
             (city: City) =>
